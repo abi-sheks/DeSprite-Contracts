201cf5a730ca1d72e3374f94173097d2e8ebb620
abishek <abishek.arun2017@gmail.com>
2023-11-08 05:04:40 +0530

    adds deploy scripts

--- script/Marketplace.s.sol
+++ script/Marketplace.s.sol
@@ -1,2 +1,17 @@
 //SPDX-License-Identifier : UNLICENSED
 pragma solidity ^0.8.20;
+
+import {Script} from 'forge-std/Script.sol';
+import {Marketplace} from '../src/Marketplace.sol';
+
+contract MarketplaceScript is Script{
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Marketplace marketplace = new Marketplace();
+
+        vm.stopBroadcast();
+
+    }
+}
--- script/NFTFactory.s.sol
+++ script/NFTFactory.s.sol
@@ -1,2 +1,17 @@
 //SPDX-License-Identifier : UNLICENSED
 pragma solidity ^0.8.20;
+
+import {Script} from 'forge-std/Script.sol';
+import {NFTFactory} from '../src/NFTFactory.sol';
+
+contract NFTFactoryScript is Script{
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        NFTFactory nft = new NFTFactory();
+
+        vm.stopBroadcast();
+
+    }
+}
